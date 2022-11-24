041618a3d899ab00cf3616997ff13d93b74fd075
jakob-a2k <jakub.a2k@gmail.com>
2022-11-24 09:17:52 +0100

    create simple phone_book for 10 users

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ struct Book{
 
 void show_menu();
 void add(Book user[],int i);
+void display(Book user[],int i);
+void edit(Book user[]);
 
 int main()
 {
@@ -26,13 +28,13 @@ int main()
             i++;
             break;
         case 2:
-            //edit();
+            edit(user);
             break;
         case 3:
-            //display();
+            display(user,i);
             break;
         case 4:
-            //delete_number();
+            i--;
             break;
         case 5:
             exit(0);
@@ -43,12 +45,13 @@ int main()
     return 0;
 }
 void show_menu(){
+    cout << endl;
     cout << "-------------PHONE BOOK-------------" << endl;
     cout << "Choose one of the available options!" << endl;
-    cout << "1. Addition" << endl;
+    cout << "1. Add" << endl;
     cout << "2. Edit" << endl;
     cout << "3. Display" << endl;
-    cout << "4. Delete" << endl;
+    cout << "4. Delete last person" << endl;
     cout << "5. Exit" << endl;
 }
 void add(Book user[], int i){
@@ -58,4 +61,24 @@ void add(Book user[], int i){
     cin >> user[i].surname;
     cout << "Enter phone" << endl;
     cin >> user[i].phone;
+}
+void display(Book user[],int i){
+    if(i == 0){
+        cout << "Phone book is emplty!" << endl;
+        cout << "Use add function!" << endl;
+    }
+    else{
+        for(int j = 0; j < i; j++){
+            cout << j+1 << ". Person" << endl;
+            cout << user[j].name << " ";
+            cout << user[j].surname << ", phone: ";
+            cout << user[j].phone << endl;
+        }
+    }
+}
+void edit(Book user[]){
+    int number;
+    cout << "Which contact you want to edit?" << endl;
+    cin >> number;
+    add(user,number-1);
 }
